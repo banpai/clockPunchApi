6e5be017d06188d5b16e7eed625b0bd3fbc64560
banpai <banpai@yeah.net>
2017-11-28 15:55:30 +0800

    修改路口文件跳转CMS后台

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -6,14 +6,9 @@ class Index
     public function index()
     {
         /**
-         * 跳转到微信开发的提交表单
+         * 跳转到后台CMS
          */
-        $redirect_uri = config('host').'/dist/html/index.html';
-        $redirect_uri = urlencode($redirect_uri);
-        $appId = config('we_appId');
-        $url = 'https://open.weixin.qq.com/connect/oauth2/authorize?appid='.$appId.'&redirect_uri='.$redirect_uri.'&response_type=code&scope=snsapi_base&state=123#wechat_redirect';
+        $url = '/view/clockpunch/cms/index_prod.html';
         return redirect($url);
-
-//        return 'd';
     }
 }
